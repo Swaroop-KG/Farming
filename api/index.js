84134f6317a33fbe6b7158735941d297798fbe7e
swaroop-12 <swaroop.k.g49@gmail.com>
2024-11-10 22:42:55 +0530

    connecting database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then( ()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((err)=>{
+    console.log(err);
+
+});
+
 
 const app = express(); 
 app.listen(3002, () => {
-    console.log("Server listening at port 3002!!");
+    console.log("Server listening at port 3002 !!");
 });
