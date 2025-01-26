4781e0974796f6563d4a93b6b8c71954f620c6d6
Swaroop.K.G <swaroop.k.g49@gmail.com>
2025-01-26 11:38:53 +0530

    Update Profile.jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Profile() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile page</h1>
       </div>
   )
 };
